611f7a5ab5ea33c0d9f23050e00ea1afa2d6f70d
ErnestLoo <160474347+ErnestLoo@users.noreply.github.com>
2024-10-01 17:51:32 +0800

    Added Game Loop with Threads

--- PlatformerTutorial/src/main/GameWindow.java
+++ PlatformerTutorial/src/main/GameWindow.java
@@ -1,5 +1,4 @@
 package main;
-   
 
 import javax.swing.JFrame;
 
@@ -10,9 +9,9 @@ public class GameWindow{
         jFrame = new JFrame();
 
         jFrame.setSize(400,400);
-        jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.add(gamePanel);
+        jFrame.setLocationRelativeTo(null);
         jFrame.setVisible(true);
-
     }
 }
